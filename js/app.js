ec8624a549974c5b23c91e0327db9047ae32ec43
Wesley Wein Chen <wesley.wein@gmail.com>
2018-09-12 03:49:12 -0300

    Adicionado a funcionalidade de virar a carta

--- js/app.js
+++ js/app.js
@@ -36,3 +36,13 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+const deck = document.querySelector('.deck');
+deck.addEventListener('click', event => {
+	const clickTarget = event.target;
+	console.log("Teste");
+	clickTarget.classList.toggle('open');
+	clickTarget.classList.toggle('show');
+});
